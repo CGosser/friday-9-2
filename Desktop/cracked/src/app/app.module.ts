69d5458985c494395ec85baaa3495ee8509c8080
Chris Gosser <rivetkopf@gmail.com>
2018-10-19 15:47:03 -0700

    improve styling on articles list, add viral component

--- Desktop/cracked/src/app/app.module.ts
+++ Desktop/cracked/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BigFeaturedComponent } from './big-featured/big-featured.component';
 import { SmallerFeaturedComponent } from './smaller-featured/smaller-featured.component';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
+import { ViralComponent } from './viral/viral.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BigFeaturedComponent,
     SmallerFeaturedComponent,
-    ArticlesListComponent
+    ArticlesListComponent,
+    ViralComponent
   ],
   imports: [
     BrowserModule
