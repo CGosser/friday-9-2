a5ee3bfdc39ca9f72c4a9fc6697e72e0d8b01a8d
Chris Gosser <rivetkopf@gmail.com>
2018-10-19 13:33:00 -0700

    add articles for the smaller featured articles and add them to the html

--- Desktop/cracked/src/app/models/article.model.ts
+++ Desktop/cracked/src/app/models/article.model.ts
@@ -1,3 +1,3 @@
 export class Article {
-  constructor(public title: string, public author: string, public date: string, public comments: number, public imageUrl: string){}
+  constructor(public title: string, public author: string, public date: string, public comments: number, public imageUrl: string, public content: string){}
 }
--- Desktop/cracked/src/app/smaller-featured/smaller-featured.component.ts
+++ Desktop/cracked/src/app/smaller-featured/smaller-featured.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from "./../models/article.model"
 
 @Component({
   selector: 'app-smaller-featured',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./smaller-featured.component.scss']
 })
 export class SmallerFeaturedComponent implements OnInit {
-
+articles: Article[] =[
+  new Article("5 movies stunts that went horribly wrong", "Mark Hill", "October 19, 2018", 12, "http://s3.crackedcdn.com/phpimages/article/0/3/8/676038_v2.jpg", "In the age of CGI, it's easy to forget how many movie stunts are real ... and re... "),
+  new Article("a strangely convincing theory about the 'halloween' series", "Tony Alpsen", "October 19, 2018", 5, "http://s3.crackedcdn.com/phpimages/article/0/3/0/676030_v2.jpg", "Here's why retconning the entire franchise lore makes total sense. "),
+  new Article("hack your way through life with these 9 convenient gadgets", "CRACKED Store", "October 19, 2018", 5, "http://s3.crackedcdn.com/phpimages/article/0/5/4/676054_v2.jpg", "These doodads are just that doogood.")
+]
   constructor() { }
 
   ngOnInit() {
