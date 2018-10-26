99079a665d50e92035aa0afbe255b6f35ddd6a26
Chris Gosser <rivetkopf@gmail.com>
2018-10-26 11:38:01 -0700

    import firebase stuff into app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BigFeaturedComponent } from './big-featured/big-featured.component';
 import { SmallerFeaturedComponent } from './smaller-featured/smaller-featured.component';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { ViralComponent } from './viral/viral.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
